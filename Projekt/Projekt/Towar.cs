41e9183a338ff6640777efd34f7ae567624d3ff2
Emil Rochminski <emilo113@gmail.com>
2016-12-18 20:22:12 +0100

    Lokalizacja + Towar

---
+++ Projekt/Projekt/Towar.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projekt
+{
+    class Towar
+    {
+        int id;
+        string nazwa;
+        int ilosc;
+        List<Lokalizacja> lokalizacje;
+
+        public Towar(int id, string nazwa, int ilosc, Lokalizacja lokalizacja)
+        {
+            this.id = id;
+            this.nazwa = nazwa;
+            this.ilosc = ilosc;
+            this.lokalizacje.Add(lokalizacja);
+        }
+
+        public void Usuńlokalizacje(int sektor, int rzad, int polka)
+        {
+            for (int i = 0; i < lokalizacje.Count; i++)
+            {
+                if (lokalizacje[i].sektor == sektor && lokalizacje[i].sektor == rzad && lokalizacje[i].polka == polka)
+                {
+                    lokalizacje.Remove(lokalizacje[i]);
+                    MessageBox.Show("Pomyślnie usunięto wskazaną lokalizację");
+                    return;
+                }
+            }
+            MessageBox.Show("Nie odnaleziono podanej lokalizacji towaru.");
+        }
+    }
+}
