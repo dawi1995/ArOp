11e5aa153189f9a9b7cfd030029ab53d1ab203c0
dawid <dawid@DESKTOP-FE8VDHN>
2016-12-18 15:56:32 +0100

    klasa Osoba

---
+++ Projekt/Projekt/Osoba.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt
+{
+    class Osoba
+    {
+        int id;
+        string imie;
+        string nazwisko;
+        string pesel;
+        int telefon;
+        DateTime dataUrodzenia;
+        public Osoba(int id, string imie, string nazwisko, string pesel, int telefon, DateTime dataUrodzenia)
+        {
+            this.id = id;
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.pesel = pesel;
+            this.telefon = telefon;
+            this.dataUrodzenia = dataUrodzenia;
+        }
+    }
+}
