43a45c00a9e3f8a2d25236a905578833dc30ffbf
dawid <dawid@DESKTOP-FE8VDHN>
2016-12-18 16:26:00 +0100

    czlonekfirmy

--- Projekt/Projekt/CzlonekFirmy.cs
+++ Projekt/Projekt/CzlonekFirmy.cs
@@ -10,6 +10,12 @@ namespace Projekt
     {
         Magazyn magazyn;
         BazaDanych bd;
+        public CzlonekFirmy(Magazyn magazyn, BazaDanych bd, int id, string imie, string nazwisko, string pesel, int telefon, DateTime dataUrodzenia)
+            : base(id, imie, nazwisko, pesel, telefon, dataUrodzenia)
+        {
+            this.magazyn = magazyn;
+            this.bd = bd;
+        }
     }
-    public CzlonekFirmy
+
 }
