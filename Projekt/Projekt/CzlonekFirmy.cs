c26c86ea0dd7d08ef591f5364820ac0b0b7e0752
dawid <dawid@DESKTOP-FE8VDHN>
2016-12-18 16:16:36 +0100

    CzlonekFirmy

---
+++ Projekt/Projekt/CzlonekFirmy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt
+{
+    class CzlonekFirmy : Osoba
+    {
+        Magazyn magazyn;
+        BazaDanych bd;
+    }
+    public CzlonekFirmy
+}
