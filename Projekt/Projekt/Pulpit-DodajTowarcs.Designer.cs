7207c3b882bce5a65265a2bb463bce080bacda78
dawid <dawid@DESKTOP-FE8VDHN>
2017-01-15 15:51:52 +0100

    GUI

--- Projekt/Projekt/Pulpit-DodajTowarcs.Designer.cs
+++ Projekt/Projekt/Pulpit-DodajTowarcs.Designer.cs
@@ -48,45 +48,55 @@
             // label_NazwaTowaru
             // 
             this.label_NazwaTowaru.AutoSize = true;
-            this.label_NazwaTowaru.Location = new System.Drawing.Point(12, 9);
+            this.label_NazwaTowaru.Font = new System.Drawing.Font("Agency FB", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_NazwaTowaru.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(123)))), ((int)(((byte)(57)))));
+            this.label_NazwaTowaru.Location = new System.Drawing.Point(12, 14);
             this.label_NazwaTowaru.Name = "label_NazwaTowaru";
-            this.label_NazwaTowaru.Size = new System.Drawing.Size(81, 13);
+            this.label_NazwaTowaru.Size = new System.Drawing.Size(102, 25);
             this.label_NazwaTowaru.TabIndex = 0;
             this.label_NazwaTowaru.Text = "Nazwa towaru :";
             // 
             // label_IdTowaru
             // 
             this.label_IdTowaru.AutoSize = true;
-            this.label_IdTowaru.Location = new System.Drawing.Point(12, 58);
+            this.label_IdTowaru.Font = new System.Drawing.Font("Agency FB", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_IdTowaru.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(123)))), ((int)(((byte)(57)))));
+            this.label_IdTowaru.Location = new System.Drawing.Point(12, 51);
             this.label_IdTowaru.Name = "label_IdTowaru";
-            this.label_IdTowaru.Size = new System.Drawing.Size(59, 13);
+            this.label_IdTowaru.Size = new System.Drawing.Size(76, 25);
             this.label_IdTowaru.TabIndex = 1;
             this.label_IdTowaru.Text = "ID towaru :";
             // 
             // label_Sektor
             // 
             this.label_Sektor.AutoSize = true;
-            this.label_Sektor.Location = new System.Drawing.Point(12, 106);
+            this.label_Sektor.Font = new System.Drawing.Font("Agency FB", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Sektor.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(123)))), ((int)(((byte)(57)))));
+            this.label_Sektor.Location = new System.Drawing.Point(12, 88);
             this.label_Sektor.Name = "label_Sektor";
-            this.label_Sektor.Size = new System.Drawing.Size(44, 13);
+            this.label_Sektor.Size = new System.Drawing.Size(58, 25);
             this.label_Sektor.TabIndex = 2;
             this.label_Sektor.Text = "Sektor :";
             // 
             // label_Rzad
             // 
             this.label_Rzad.AutoSize = true;
-            this.label_Rzad.Location = new System.Drawing.Point(12, 154);
+            this.label_Rzad.Font = new System.Drawing.Font("Agency FB", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Rzad.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(123)))), ((int)(((byte)(57)))));
+            this.label_Rzad.Location = new System.Drawing.Point(12, 125);
             this.label_Rzad.Name = "label_Rzad";
-            this.label_Rzad.Size = new System.Drawing.Size(38, 13);
+            this.label_Rzad.Size = new System.Drawing.Size(49, 25);
             this.label_Rzad.TabIndex = 3;
-            this.label_Rzad.Text = "Rząd :";
+            this.label_Rzad.Text = "Rzad :";
             // 
             // label_Polka
             // 
             this.label_Polka.AutoSize = true;
-            this.label_Polka.Location = new System.Drawing.Point(12, 205);
+            this.label_Polka.Font = new System.Drawing.Font("Agency FB", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Polka.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(123)))), ((int)(((byte)(57)))));
+            this.label_Polka.Location = new System.Drawing.Point(10, 162);
             this.label_Polka.Name = "label_Polka";
-            this.label_Polka.Size = new System.Drawing.Size(42, 13);
+            this.label_Polka.Size = new System.Drawing.Size(51, 25);
             this.label_Polka.TabIndex = 4;
             this.label_Polka.Text = "Półka :";
             // 
@@ -103,7 +113,7 @@
             // 
             this.textBox_idtowaru.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(199)))), ((int)(((byte)(199)))));
             this.textBox_idtowaru.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBox_idtowaru.Location = new System.Drawing.Point(171, 52);
+            this.textBox_idtowaru.Location = new System.Drawing.Point(157, 49);
             this.textBox_idtowaru.Name = "textBox_idtowaru";
             this.textBox_idtowaru.Size = new System.Drawing.Size(215, 31);
             this.textBox_idtowaru.TabIndex = 6;
@@ -112,7 +122,7 @@
             // 
             this.textBox_sektor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(199)))), ((int)(((byte)(199)))));
             this.textBox_sektor.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBox_sektor.Location = new System.Drawing.Point(171, 103);
+            this.textBox_sektor.Location = new System.Drawing.Point(157, 86);
             this.textBox_sektor.Name = "textBox_sektor";
             this.textBox_sektor.Size = new System.Drawing.Size(215, 31);
             this.textBox_sektor.TabIndex = 7;
@@ -121,7 +131,7 @@
             // 
             this.textBox_rzad.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(199)))), ((int)(((byte)(199)))));
             this.textBox_rzad.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBox_rzad.Location = new System.Drawing.Point(171, 148);
+            this.textBox_rzad.Location = new System.Drawing.Point(157, 123);
             this.textBox_rzad.Name = "textBox_rzad";
             this.textBox_rzad.Size = new System.Drawing.Size(215, 31);
             this.textBox_rzad.TabIndex = 8;
@@ -130,7 +140,7 @@
             // 
             this.textBox_polka.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(199)))), ((int)(((byte)(199)))));
             this.textBox_polka.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBox_polka.Location = new System.Drawing.Point(171, 199);
+            this.textBox_polka.Location = new System.Drawing.Point(157, 160);
             this.textBox_polka.Name = "textBox_polka";
             this.textBox_polka.Size = new System.Drawing.Size(215, 31);
             this.textBox_polka.TabIndex = 9;
@@ -152,17 +162,19 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 256);
+            this.label1.Font = new System.Drawing.Font("Agency FB", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(123)))), ((int)(((byte)(57)))));
+            this.label1.Location = new System.Drawing.Point(10, 199);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(47, 25);
             this.label1.TabIndex = 11;
-            this.label1.Text = "Ilość :";
+            this.label1.Text = "Ilosc :";
             // 
             // textBox_ilosc
             // 
             this.textBox_ilosc.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(199)))), ((int)(((byte)(199)))));
             this.textBox_ilosc.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBox_ilosc.Location = new System.Drawing.Point(171, 253);
+            this.textBox_ilosc.Location = new System.Drawing.Point(157, 197);
             this.textBox_ilosc.Name = "textBox_ilosc";
             this.textBox_ilosc.Size = new System.Drawing.Size(215, 31);
             this.textBox_ilosc.TabIndex = 12;
@@ -170,9 +182,11 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 304);
+            this.label2.Font = new System.Drawing.Font("Agency FB", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(123)))), ((int)(((byte)(57)))));
+            this.label2.Location = new System.Drawing.Point(10, 236);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.Size = new System.Drawing.Size(77, 25);
             this.label2.TabIndex = 13;
             this.label2.Text = "Informacje:";
             // 
@@ -180,7 +194,7 @@
             // 
             this.textBox_info.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(199)))), ((int)(((byte)(199)))));
             this.textBox_info.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBox_info.Location = new System.Drawing.Point(171, 297);
+            this.textBox_info.Location = new System.Drawing.Point(157, 234);
             this.textBox_info.Name = "textBox_info";
             this.textBox_info.Size = new System.Drawing.Size(215, 31);
             this.textBox_info.TabIndex = 14;
