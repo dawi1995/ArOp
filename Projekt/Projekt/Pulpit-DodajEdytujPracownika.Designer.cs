3e0400c3520d67546fc48705e5644142db33563a
dawid <dawid@DESKTOP-FE8VDHN>
2017-01-15 15:23:41 +0100

    GUI

--- Projekt/Projekt/Pulpit-DodajEdytujPracownika.Designer.cs
+++ Projekt/Projekt/Pulpit-DodajEdytujPracownika.Designer.cs
@@ -38,105 +38,118 @@
             this.textBox_PESEL = new System.Windows.Forms.TextBox();
             this.textBox_telefon = new System.Windows.Forms.TextBox();
             this.button_DodajPracownika = new System.Windows.Forms.Button();
-            this.button_EdytujPracownika = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label_ID = new System.Windows.Forms.Label();
             this.textBox_ID = new System.Windows.Forms.TextBox();
             this.textBox_date = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label_Imie
             // 
             this.label_Imie.AutoSize = true;
-            this.label_Imie.Location = new System.Drawing.Point(125, 81);
+            this.label_Imie.Font = new System.Drawing.Font("Agency FB", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Imie.ForeColor = System.Drawing.Color.Coral;
+            this.label_Imie.Location = new System.Drawing.Point(9, 53);
             this.label_Imie.Name = "label_Imie";
-            this.label_Imie.Size = new System.Drawing.Size(32, 13);
+            this.label_Imie.Size = new System.Drawing.Size(44, 25);
             this.label_Imie.TabIndex = 0;
-            this.label_Imie.Text = "Imię :";
+            this.label_Imie.Text = "Imie :";
             // 
             // label_Nazwisko
             // 
             this.label_Nazwisko.AutoSize = true;
-            this.label_Nazwisko.Location = new System.Drawing.Point(125, 128);
+            this.label_Nazwisko.Font = new System.Drawing.Font("Agency FB", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Nazwisko.ForeColor = System.Drawing.Color.Coral;
+            this.label_Nazwisko.Location = new System.Drawing.Point(9, 91);
             this.label_Nazwisko.Name = "label_Nazwisko";
-            this.label_Nazwisko.Size = new System.Drawing.Size(59, 13);
+            this.label_Nazwisko.Size = new System.Drawing.Size(74, 25);
             this.label_Nazwisko.TabIndex = 1;
             this.label_Nazwisko.Text = "Nazwisko :";
             // 
             // label_PESEL
             // 
             this.label_PESEL.AutoSize = true;
-            this.label_PESEL.Location = new System.Drawing.Point(125, 173);
+            this.label_PESEL.Font = new System.Drawing.Font("Agency FB", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_PESEL.ForeColor = System.Drawing.Color.Coral;
+            this.label_PESEL.Location = new System.Drawing.Point(9, 129);
             this.label_PESEL.Name = "label_PESEL";
-            this.label_PESEL.Size = new System.Drawing.Size(47, 13);
+            this.label_PESEL.Size = new System.Drawing.Size(59, 25);
             this.label_PESEL.TabIndex = 2;
             this.label_PESEL.Text = "PESEL :";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(125, 220);
+            this.label4.Font = new System.Drawing.Font("Agency FB", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Coral;
+            this.label4.Location = new System.Drawing.Point(9, 167);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(79, 13);
+            this.label4.Size = new System.Drawing.Size(104, 25);
             this.label4.TabIndex = 3;
-            this.label4.Text = "Data urodzenia";
+            this.label4.Text = "Data urodzenia:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(125, 259);
+            this.label5.Font = new System.Drawing.Font("Agency FB", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Coral;
+            this.label5.Location = new System.Drawing.Point(9, 230);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.Size = new System.Drawing.Size(55, 25);
             this.label5.TabIndex = 4;
-            this.label5.Text = "Telefon";
+            this.label5.Text = "Telefon:";
             // 
             // textBox_imie
             // 
-            this.textBox_imie.Location = new System.Drawing.Point(293, 78);
+            this.textBox_imie.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(199)))), ((int)(((byte)(199)))));
+            this.textBox_imie.Font = new System.Drawing.Font("Agency FB", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_imie.Location = new System.Drawing.Point(157, 50);
             this.textBox_imie.Name = "textBox_imie";
-            this.textBox_imie.Size = new System.Drawing.Size(261, 20);
+            this.textBox_imie.Size = new System.Drawing.Size(215, 32);
             this.textBox_imie.TabIndex = 5;
             // 
             // textBox_nazwisko
             // 
-            this.textBox_nazwisko.Location = new System.Drawing.Point(293, 125);
+            this.textBox_nazwisko.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(199)))), ((int)(((byte)(199)))));
+            this.textBox_nazwisko.Font = new System.Drawing.Font("Agency FB", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_nazwisko.Location = new System.Drawing.Point(157, 88);
             this.textBox_nazwisko.Name = "textBox_nazwisko";
-            this.textBox_nazwisko.Size = new System.Drawing.Size(261, 20);
+            this.textBox_nazwisko.Size = new System.Drawing.Size(215, 32);
             this.textBox_nazwisko.TabIndex = 6;
             // 
             // textBox_PESEL
             // 
-            this.textBox_PESEL.Location = new System.Drawing.Point(293, 170);
+            this.textBox_PESEL.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(199)))), ((int)(((byte)(199)))));
+            this.textBox_PESEL.Font = new System.Drawing.Font("Agency FB", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_PESEL.Location = new System.Drawing.Point(157, 126);
             this.textBox_PESEL.Name = "textBox_PESEL";
-            this.textBox_PESEL.Size = new System.Drawing.Size(261, 20);
+            this.textBox_PESEL.Size = new System.Drawing.Size(215, 32);
             this.textBox_PESEL.TabIndex = 7;
             // 
             // textBox_telefon
             // 
-            this.textBox_telefon.Location = new System.Drawing.Point(293, 256);
+            this.textBox_telefon.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(199)))), ((int)(((byte)(199)))));
+            this.textBox_telefon.Font = new System.Drawing.Font("Agency FB", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_telefon.Location = new System.Drawing.Point(157, 227);
             this.textBox_telefon.Name = "textBox_telefon";
-            this.textBox_telefon.Size = new System.Drawing.Size(261, 20);
+            this.textBox_telefon.Size = new System.Drawing.Size(215, 32);
             this.textBox_telefon.TabIndex = 9;
             // 
             // button_DodajPracownika
             // 
-            this.button_DodajPracownika.Location = new System.Drawing.Point(139, 338);
+            this.button_DodajPracownika.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(123)))), ((int)(((byte)(57)))));
+            this.button_DodajPracownika.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button_DodajPracownika.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_DodajPracownika.Font = new System.Drawing.Font("Agency FB", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_DodajPracownika.Location = new System.Drawing.Point(105, 300);
             this.button_DodajPracownika.Name = "button_DodajPracownika";
-            this.button_DodajPracownika.Size = new System.Drawing.Size(189, 23);
+            this.button_DodajPracownika.Size = new System.Drawing.Size(180, 40);
             this.button_DodajPracownika.TabIndex = 11;
             this.button_DodajPracownika.Text = "Dodaj pracownika";
-            this.button_DodajPracownika.UseVisualStyleBackColor = true;
+            this.button_DodajPracownika.UseVisualStyleBackColor = false;
             this.button_DodajPracownika.Click += new System.EventHandler(this.button_DodajPracownika_Click);
-            // 
-            // button_EdytujPracownika
-            // 
-            this.button_EdytujPracownika.Location = new System.Drawing.Point(353, 338);
-            this.button_EdytujPracownika.Name = "button_EdytujPracownika";
-            this.button_EdytujPracownika.Size = new System.Drawing.Size(189, 23);
-            this.button_EdytujPracownika.TabIndex = 12;
-            this.button_EdytujPracownika.Text = "Edytuj pracownika";
-            this.button_EdytujPracownika.UseVisualStyleBackColor = true;
-            this.button_EdytujPracownika.Click += new System.EventHandler(this.button_EdytujPracownika_Click);
             // 
             // label1
             // 
@@ -150,36 +163,68 @@
             // label_ID
             // 
             this.label_ID.AutoSize = true;
-            this.label_ID.Location = new System.Drawing.Point(128, 40);
+            this.label_ID.Font = new System.Drawing.Font("Agency FB", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_ID.ForeColor = System.Drawing.Color.Coral;
+            this.label_ID.Location = new System.Drawing.Point(12, 15);
             this.label_ID.Name = "label_ID";
-            this.label_ID.Size = new System.Drawing.Size(24, 13);
+            this.label_ID.Size = new System.Drawing.Size(32, 25);
             this.label_ID.TabIndex = 14;
             this.label_ID.Text = "ID :";
             // 
             // textBox_ID
             // 
-            this.textBox_ID.Location = new System.Drawing.Point(293, 37);
+            this.textBox_ID.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(199)))), ((int)(((byte)(199)))));
+            this.textBox_ID.Font = new System.Drawing.Font("Agency FB", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_ID.Location = new System.Drawing.Point(157, 12);
             this.textBox_ID.Name = "textBox_ID";
-            this.textBox_ID.Size = new System.Drawing.Size(261, 20);
+            this.textBox_ID.Size = new System.Drawing.Size(215, 32);
             this.textBox_ID.TabIndex = 15;
             // 
             // textBox_date
             // 
-            this.textBox_date.Location = new System.Drawing.Point(293, 212);
+            this.textBox_date.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(199)))), ((int)(((byte)(199)))));
+            this.textBox_date.Font = new System.Drawing.Font("Agency FB", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_date.Location = new System.Drawing.Point(157, 164);
             this.textBox_date.Name = "textBox_date";
-            this.textBox_date.Size = new System.Drawing.Size(261, 20);
+            this.textBox_date.Size = new System.Drawing.Size(215, 32);
             this.textBox_date.TabIndex = 16;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
+            this.label2.Font = new System.Drawing.Font("Agency FB", 10F);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(199)))), ((int)(((byte)(199)))));
+            this.label2.Location = new System.Drawing.Point(154, 199);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 18);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Format: RRRR-MM-DD";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
+            this.label3.Font = new System.Drawing.Font("Agency FB", 10F);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(199)))), ((int)(((byte)(199)))));
+            this.label3.Location = new System.Drawing.Point(154, 262);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 18);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Format: XXXXXXXXX";
             // 
             // Pulpit_DodajEdytujPracownika
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(664, 442);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox_date);
             this.Controls.Add(this.textBox_ID);
             this.Controls.Add(this.label_ID);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button_EdytujPracownika);
             this.Controls.Add(this.button_DodajPracownika);
             this.Controls.Add(this.textBox_telefon);
             this.Controls.Add(this.textBox_PESEL);
@@ -190,6 +235,7 @@
             this.Controls.Add(this.label_PESEL);
             this.Controls.Add(this.label_Nazwisko);
             this.Controls.Add(this.label_Imie);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Pulpit_DodajEdytujPracownika";
             this.Text = "Dodaj/edytuj pracownika";
             this.ResumeLayout(false);
@@ -209,10 +255,11 @@
         private System.Windows.Forms.TextBox textBox_PESEL;
         private System.Windows.Forms.TextBox textBox_telefon;
         private System.Windows.Forms.Button button_DodajPracownika;
-        private System.Windows.Forms.Button button_EdytujPracownika;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label_ID;
         private System.Windows.Forms.TextBox textBox_ID;
         private System.Windows.Forms.TextBox textBox_date;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
