80f68aabcebcd1c78a37a202e9b80933b8f7b9c2
dawid <dawid@DESKTOP-FE8VDHN>
2017-01-15 16:00:22 +0100

    GUI - usunpracownika

--- Projekt/Projekt/Pulpit-UsunPracownika.Designer.cs
+++ Projekt/Projekt/Pulpit-UsunPracownika.Designer.cs
@@ -35,29 +35,36 @@
             // 
             // button_UsunPracownika
             // 
+            this.button_UsunPracownika.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(123)))), ((int)(((byte)(57)))));
+            this.button_UsunPracownika.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button_UsunPracownika.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button_UsunPracownika.Font = new System.Drawing.Font("Agency FB", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button_UsunPracownika.Location = new System.Drawing.Point(105, 100);
             this.button_UsunPracownika.Name = "button_UsunPracownika";
             this.button_UsunPracownika.Size = new System.Drawing.Size(180, 40);
             this.button_UsunPracownika.TabIndex = 14;
             this.button_UsunPracownika.Text = "Usun pracownika";
-            this.button_UsunPracownika.UseVisualStyleBackColor = true;
+            this.button_UsunPracownika.UseVisualStyleBackColor = false;
             this.button_UsunPracownika.Click += new System.EventHandler(this.button_UsunPracownika_Click);
             // 
             // label_ID
             // 
             this.label_ID.AutoSize = true;
-            this.label_ID.Location = new System.Drawing.Point(23, 29);
+            this.label_ID.Font = new System.Drawing.Font("Agency FB", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_ID.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(123)))), ((int)(((byte)(57)))));
+            this.label_ID.Location = new System.Drawing.Point(12, 15);
             this.label_ID.Name = "label_ID";
-            this.label_ID.Size = new System.Drawing.Size(24, 13);
+            this.label_ID.Size = new System.Drawing.Size(32, 25);
             this.label_ID.TabIndex = 15;
             this.label_ID.Text = "ID :";
             // 
             // textbox_ID
             // 
-            this.textbox_ID.Location = new System.Drawing.Point(89, 22);
+            this.textbox_ID.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(199)))), ((int)(((byte)(199)))));
+            this.textbox_ID.Font = new System.Drawing.Font("Agency FB", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textbox_ID.Location = new System.Drawing.Point(157, 12);
             this.textbox_ID.Name = "textbox_ID";
-            this.textbox_ID.Size = new System.Drawing.Size(261, 20);
+            this.textbox_ID.Size = new System.Drawing.Size(215, 32);
             this.textbox_ID.TabIndex = 16;
             // 
             // Pulpit_UsunPracownika
