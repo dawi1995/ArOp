ca5933dbcdb9c91405d04ec389b10b7825bfcaac
dawid <dawid@DESKTOP-FE8VDHN>
2017-01-15 14:32:13 +0100

    zmiany

--- Projekt/Projekt/Pulpit-Pracownik.Designer.cs
+++ Projekt/Projekt/Pulpit-Pracownik.Designer.cs
@@ -35,55 +35,76 @@
             this.button_SprawdzGrafik = new System.Windows.Forms.Button();
             this.button_Wyloguj = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button_DodajTowar
             // 
-            this.button_DodajTowar.Location = new System.Drawing.Point(890, 77);
+            this.button_DodajTowar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(123)))), ((int)(((byte)(57)))));
+            this.button_DodajTowar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button_DodajTowar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_DodajTowar.Font = new System.Drawing.Font("Agency FB", 15.75F);
+            this.button_DodajTowar.Location = new System.Drawing.Point(10, 10);
             this.button_DodajTowar.Name = "button_DodajTowar";
-            this.button_DodajTowar.Size = new System.Drawing.Size(327, 23);
+            this.button_DodajTowar.Size = new System.Drawing.Size(180, 40);
             this.button_DodajTowar.TabIndex = 1;
             this.button_DodajTowar.Text = "Dodaj nowy towar";
-            this.button_DodajTowar.UseVisualStyleBackColor = true;
+            this.button_DodajTowar.UseVisualStyleBackColor = false;
             this.button_DodajTowar.Click += new System.EventHandler(this.button_DodajTowar_Click);
             // 
             // button_DodajIstTowar
             // 
-            this.button_DodajIstTowar.Location = new System.Drawing.Point(890, 143);
+            this.button_DodajIstTowar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(123)))), ((int)(((byte)(57)))));
+            this.button_DodajIstTowar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button_DodajIstTowar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_DodajIstTowar.Font = new System.Drawing.Font("Agency FB", 15.75F);
+            this.button_DodajIstTowar.Location = new System.Drawing.Point(10, 52);
             this.button_DodajIstTowar.Name = "button_DodajIstTowar";
-            this.button_DodajIstTowar.Size = new System.Drawing.Size(327, 23);
+            this.button_DodajIstTowar.Size = new System.Drawing.Size(180, 40);
             this.button_DodajIstTowar.TabIndex = 2;
-            this.button_DodajIstTowar.Text = "Dodaj istniejący towar";
-            this.button_DodajIstTowar.UseVisualStyleBackColor = true;
+            this.button_DodajIstTowar.Text = "Dodaj istniejacy towar";
+            this.button_DodajIstTowar.UseVisualStyleBackColor = false;
             this.button_DodajIstTowar.Click += new System.EventHandler(this.button_DodajIstTowar_Click);
             // 
             // button_UsuńTowar
             // 
-            this.button_UsuńTowar.Location = new System.Drawing.Point(890, 214);
+            this.button_UsuńTowar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(123)))), ((int)(((byte)(57)))));
+            this.button_UsuńTowar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button_UsuńTowar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_UsuńTowar.Font = new System.Drawing.Font("Agency FB", 15.75F);
+            this.button_UsuńTowar.Location = new System.Drawing.Point(10, 94);
             this.button_UsuńTowar.Name = "button_UsuńTowar";
-            this.button_UsuńTowar.Size = new System.Drawing.Size(327, 23);
+            this.button_UsuńTowar.Size = new System.Drawing.Size(180, 40);
             this.button_UsuńTowar.TabIndex = 3;
-            this.button_UsuńTowar.Text = "Usuń towar";
-            this.button_UsuńTowar.UseVisualStyleBackColor = true;
+            this.button_UsuńTowar.Text = "Usun towar";
+            this.button_UsuńTowar.UseVisualStyleBackColor = false;
             this.button_UsuńTowar.Click += new System.EventHandler(this.button_UsuńTowar_Click);
             // 
             // button_SprawdzZlecenia
             // 
-            this.button_SprawdzZlecenia.Location = new System.Drawing.Point(890, 451);
+            this.button_SprawdzZlecenia.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(123)))), ((int)(((byte)(57)))));
+            this.button_SprawdzZlecenia.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button_SprawdzZlecenia.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_SprawdzZlecenia.Font = new System.Drawing.Font("Agency FB", 15.75F);
+            this.button_SprawdzZlecenia.Location = new System.Drawing.Point(11, 348);
             this.button_SprawdzZlecenia.Name = "button_SprawdzZlecenia";
-            this.button_SprawdzZlecenia.Size = new System.Drawing.Size(327, 23);
+            this.button_SprawdzZlecenia.Size = new System.Drawing.Size(180, 40);
             this.button_SprawdzZlecenia.TabIndex = 5;
             this.button_SprawdzZlecenia.Text = "Sprawdz swoje zlecenia";
-            this.button_SprawdzZlecenia.UseVisualStyleBackColor = true;
+            this.button_SprawdzZlecenia.UseVisualStyleBackColor = false;
             // 
             // button_SprawdzGrafik
             // 
-            this.button_SprawdzGrafik.Location = new System.Drawing.Point(890, 502);
+            this.button_SprawdzGrafik.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(123)))), ((int)(((byte)(57)))));
+            this.button_SprawdzGrafik.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button_SprawdzGrafik.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_SprawdzGrafik.Font = new System.Drawing.Font("Agency FB", 15.75F);
+            this.button_SprawdzGrafik.Location = new System.Drawing.Point(11, 390);
             this.button_SprawdzGrafik.Name = "button_SprawdzGrafik";
-            this.button_SprawdzGrafik.Size = new System.Drawing.Size(327, 23);
+            this.button_SprawdzGrafik.Size = new System.Drawing.Size(180, 40);
             this.button_SprawdzGrafik.TabIndex = 6;
-            this.button_SprawdzGrafik.Text = "Wyświetl grafik";
-            this.button_SprawdzGrafik.UseVisualStyleBackColor = true;
+            this.button_SprawdzGrafik.Text = "Wyswietl grafik";
+            this.button_SprawdzGrafik.UseVisualStyleBackColor = false;
             // 
             // button_Wyloguj
             // 
@@ -99,6 +120,12 @@
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button_DodajTowar);
+            this.panel1.Controls.Add(this.button_DodajIstTowar);
+            this.panel1.Controls.Add(this.button_SprawdzGrafik);
+            this.panel1.Controls.Add(this.button_UsuńTowar);
+            this.panel1.Controls.Add(this.button_SprawdzZlecenia);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(200, 478);
@@ -108,16 +135,13 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1281, 582);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(199)))), ((int)(((byte)(199)))));
+            this.ClientSize = new System.Drawing.Size(871, 478);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.button_Wyloguj);
-            this.Controls.Add(this.button_SprawdzGrafik);
-            this.Controls.Add(this.button_SprawdzZlecenia);
-            this.Controls.Add(this.button_UsuńTowar);
-            this.Controls.Add(this.button_DodajIstTowar);
-            this.Controls.Add(this.button_DodajTowar);
             this.Name = "Pulpit_Pracownik";
             this.Text = "Pulpit_Pracownik";
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
